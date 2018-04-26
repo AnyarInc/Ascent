627eebb2b9f8f4e4d54a810d4de60e6bb29ac234
Stephen Berry <stephenberry.developer@gmail.com>
2018-04-26 12:52:42 -0500

    Added Pool and Queue

---
+++ ascent/threading/Pool.h
@@ -0,0 +1,60 @@
+// Copyright (c) 2016-2018 Anyar, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "ascent/threading/Queue.h"
+
+// a multi-threaded pool of queues, with simple load balancing
+
+namespace asc
+{
+   struct Pool final
+   {
+      std::vector<std::unique_ptr<Queue>> pool;
+
+      void n_threads(const size_t n)
+      {
+         for (size_t i = pool.size(); i < n; ++i)
+            pool.emplace_back(std::make_unique<Queue>());
+      }
+
+      template <typename ...Args>
+      void emplace_back(Args&&... args)
+      {
+         if (pool.empty())
+            pool.emplace_back(std::make_unique<Queue>());
+         auto compare = [](auto& l, auto& r) { return l->size() < r->size(); };
+         auto it = std::min_element(pool.begin(), pool.end(), compare);
+         (*it)->emplace_back(args...);
+      }
+
+      size_t size() const
+      {
+         return pool.size();
+      }
+
+      template <typename ChaiScript>
+      static void script(ChaiScript& c, const std::string& name)
+      {
+         using namespace chaiscript;
+         using T = Pool;
+         c.add(constructor<T()>(), name);
+
+         c.add(fun(&T::n_threads), "n_threads");
+         c.add(fun(&T::emplace_back<std::function<void()>>), "emplace_back");
+         c.add(fun(&T::size), "size");
+      }
+   };
+}
---
+++ ascent/threading/Queue.h
@@ -0,0 +1,110 @@
+// Copyright (c) 2016-2018 Anyar, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <deque>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+// A threaded worker queue
+
+namespace asc
+{
+   struct Queue final
+   {
+      Queue() = default;
+      Queue(const Queue&) = default;
+      Queue(Queue&&) = default;
+      Queue& operator=(const Queue&) = default;
+      Queue& operator=(Queue&&) = default;
+      ~Queue() { run = false; }
+
+      bool computing() const noexcept
+      {
+         return running_jobs;
+      }
+
+      template <typename ...Args>
+      void emplace_back(Args&&... args)
+      {
+         adding = true;
+         jobs.emplace_back(std::forward<Args>(args)...);
+         adding = false;
+         if (!started)
+         {
+            start();
+            started = true;
+         }
+         cv.notify_one();
+      }
+
+      size_t size() const
+      {
+         return jobs.size();
+      }
+
+      template <typename ChaiScript>
+      static void script(ChaiScript& c, const std::string& name)
+      {
+         using namespace chaiscript;
+         using T = Queue;
+         c.add(constructor<T()>(), name);
+
+         c.add(fun(&T::computing), "computing");
+         c.add(fun(&T::emplace_back<std::function<void()>>), "emplace_back");
+         c.add(fun(&T::size), "size");
+      }
+
+   private:
+      void start()
+      {
+         run = true;
+
+         auto runner = [&]
+         {
+            while (run)
+            {
+               running_jobs = true;
+               while (jobs.size() > 0)
+               {
+                  jobs.front()();
+
+                  while (adding)
+                     std::this_thread::yield();
+                  jobs.pop_front();
+               }
+               running_jobs = false;
+
+               std::unique_lock<std::mutex> lock(m);
+               cv.wait(lock);
+            }
+         };
+
+         std::thread(runner).detach();
+      }
+
+      std::mutex m;
+      bool started{};
+      std::atomic<bool> run{}, adding{}, running_jobs{};
+      std::condition_variable cv;
+      std::deque<std::function<void()>> jobs;
+   };
+}
