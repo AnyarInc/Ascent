6b7cbc6165cb78579ac83bf0ca7fc0519c54c43a
Stephen Berry <stephenberry.developer@gmail.com>
2019-02-11 10:39:21 -0600

    Added direct RK4.h integrator and State.h - This is for an significantly different approach to modular numerical integration - Using this approach loses vectorization, but makes it possible to work with states directly rather than asc::Param and asc::ParamV.

---
+++ include/ascent/direct/State.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2016-2019 Anyar, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+namespace asc
+{
+   struct State
+   {
+      State(double& x, double& xd) noexcept : x(x), xd(xd) {}
+      State(const State&) = default;
+      State(State&&) = default;
+      State& operator=(const State&) = default;
+      State& operator=(State&&) = default;
+
+      double& x;
+      double& xd;
+   };
+}
